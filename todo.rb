1747fea4256301c4b741ace3e5c187599cad7aaf
yamini692 <yaminithya692@gmail.com>
2025-06-12 10:12:28 +0530

    Add feature is added

--- todo.rb
+++ todo.rb
@@ -1,1 +1,12 @@
 puts "Welcome to Todo App"
+puts
+puts "Choose 1. Add 2. List"
+puts "Enter a Choice : "
+n = gets.chomp.to_i
+if n==1
+  tasks = []
+  puts "Enter a task:"
+  task = gets.chomp
+  tasks << task
+  puts "Task added!"
+end
