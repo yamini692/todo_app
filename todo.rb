5a2e24751ccc01b9e5faf5276203d0d59e4c6556
yamini692 <yaminithya692@gmail.com>
2025-06-12 10:43:10 +0530

    Adding listing feature

--- todo.rb
+++ todo.rb
@@ -1,12 +1,29 @@
 puts "Welcome to Todo App"
-puts
-puts "Choose 1. Add 2. List"
-puts "Enter a Choice : "
-n = gets.chomp.to_i
-if n==1
-  tasks = []
-  puts "Enter a task:"
-  task = gets.chomp
-  tasks << task
-  puts "Task added!"
+
+tasks = []
+num = 0
+
+while num != 3
+  puts
+  print "Choose: 1. Add  2. List  3. Exit: "
+  num = gets.chomp.to_i
+
+  case num
+  when 1
+    print "Enter a task: "
+    task = gets.chomp
+    tasks << task
+    puts "Task added!"
+  when 2
+    if tasks.empty?
+      puts "No tasks yet."
+    else
+      puts "Your Tasks:"
+      tasks.each_with_index { |t, i| puts "#{i + 1}. #{t}" }
+    end
+  when 3
+    puts "Exiting... Goodbye!"
+  else
+    puts "Invalid choice. Please choose 1, 2, or 3."
+  end
 end
